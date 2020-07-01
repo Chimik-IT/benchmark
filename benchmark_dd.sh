678e6c7bbea81af780d20d344f12d99745381e7d
Tobias Speer <tspeer@ls-speer>
2020-07-01 17:10:10 +0200

    changed path & rm file.img

--- benchmark_dd.sh
+++ benchmark_dd.sh
@@ -3,11 +3,11 @@ file=$1
 write="${file}_write"
 read="${file}_read"
 for i in {1..100}; do
-    dd if=/dev/urandom of=/zpool0-z3/file.img bs=1G count=1 2> test && tail -f test >>"${file}" && sed 's/,/\./g'"${file}" >> "${write}"
+    dd if=/dev/zero of=/alkoven/file.img bs=1G count=1 2> test && tail -f test >>"${file}" && sed 's/,/\./g'"${file}" >> "${write}"
 done
 
 for i in {1..100};do
-    dd if=/zpool0-z3/file.img of=/dev/zero bs=1G count=1 2> test && tail -f test >>"${file}" && sed 's/,/\./g' "${file}"  >> "${read}"
+    dd if=/alkoven/file.img of=/dev/zero bs=1G count=1 2> test && tail -f test >>"${file}" && sed 's/,/\./g' "${file}"  >> "${read}"
 done
 
 awk '{sum += $10}END{if (NR>0) print sum/NR}' "${write}" > "${write}_RESULTS"
@@ -15,6 +15,6 @@ awk '{sum += $10}END{if (NR>0) print sum/NR}' "${read}" > "${read}_RESULTS"
 
 echo -e "Write Bench: ${write}_RESULTS "
 echo -e "Write Bench: ${read}_RESULTS "
-
+rm /alkoven/file.img
 echo DONE!
 echo Have a nice Day!
